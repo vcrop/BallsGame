d40978a45a24d4dbb776a372554a1f2c1de4e753
vcrop <viktor.raguev@gmail.com>
2022-04-14 17:12:45 +0300

    initial

---
+++ src/main/java/ru/vcrop/ballsgame/BallsGameApplication.java
@@ -0,0 +1,13 @@
+package ru.vcrop.ballsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallsGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallsGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vcrop/ballsgame/BallsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vcrop.ballsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
