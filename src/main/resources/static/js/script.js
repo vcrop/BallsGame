5a83271ddbc26172002346b89abae0e753a5ec3f
vcrop <viktor.raguev@gmail.com>
2022-04-15 00:46:27 +0300

    refactor

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -15,15 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
             box.row = row;
             box.col = col;
             box.style.gridArea = (row + 1) + '/' + (col + 1) + '/' + (row + 2) + '/' + (col + 2);
+            box.onclick = () => click(box);
 
             container.appendChild(box);
 
         }
 
-    let ball = document.createElement('div');
-    ball.className = 'ball red';
-    document.getElementById('box-0_0').appendChild(ball);
+    let balls = [{color: 'yellow', row: 3, col: 3}, {color: 'red', row: 5, col: 8}, {color: 'blue', row: 1, col: 5},
+        {color: 'green', row: 5, col: 3}];
+
+    born(balls);
 
 
 })
 
+function born(balls) {
+    balls.forEach(_ball => {
+
+        let ball = document.createElement('div');
+        ball.className = 'ball ' + _ball.color;
+
+        ball.style.animation = born_animation;
+
+        document.getElementById('box-' + _ball.row + '_' + _ball.col).appendChild(ball);
+
+    });
+
+}
+
+
+function click(box) {
+
+    console.log(box.row + '_' + box.col);
+}
---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,2 @@
+const born_animation = 'born 500ms forwards';
+const select_animation = 'selection 300ms infinite forwards'
